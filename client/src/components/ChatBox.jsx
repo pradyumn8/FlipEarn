fd01d8ea58b8ecd19613a097c6ab124b546fc476
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2025-12-20 22:26:12 +0530

    created messages layout

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
+import React, { use, useEffect, useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { dummyChats } from '../assets/assets';
-import { X } from 'lucide-react';
+import { Loader2Icon, Send, X } from 'lucide-react';
+import { clearChat } from '../app/feature/chatSlice';
+import { format } from 'date-fns';
 
 const ChatBox = () => {
 
     const { listing, isOpen, chatId } = useSelector((state) => state.chat)
+    const dispatch = useDispatch();
 
     const user = { id: 'user_2' };
 
@@ -27,6 +30,29 @@ const ChatBox = () => {
         }
     }, [listing])
 
+    useEffect(() => {
+        if (!isOpen) {
+            setChat(null);
+            setMessages([]);
+            setNewMessage("");
+            setIsLoading(true);
+            setIsSending(false);
+        }
+    }, [isOpen])
+
+    //-----for Auto scroll-----
+    const messagesEndRef = useRef(null)
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+    }, [messages.length])
+
+    const handleSendMessage = async (e)=> {
+        e.preventDefault();
+        if(!newMessage.trim() || isSending) return;
+        setMessages([...messages, {id: Date.now(), chatId:chat.id, sender_id: user.id, message: newMessage, creditedAt: new Date()}]);
+        setNewMessage("")
+    }
+
     if (!isOpen || !listing) return null
 
     return (
@@ -34,11 +60,79 @@ const ChatBox = () => {
             <div className='bg-white sm:rounded-lg shodow-2xl w-full max-w-2xl h-screen sm:h-[600px] flex flex-col'>
                 {/* Header */}
                 <div className='bg-gradient-to-r from-indigo-600 to-indigo-400 text-white p-4 sm:rounded-t-lg flex items-center justify-between'>
-                    <div></div>
-                    <button className='ml-4 p-1 hover:bg-white/20 hover:bg-opacity-20 rounded-lg transition-colors'>
+                    <div className="flex-1 min-w-0">
+                        <h3 className="font-semibold text-lg truncate">
+                            {listing?.title}
+                        </h3>
+
+                        <p className="text-sm text-indigo-100 truncate">
+                            {user.id === listing?.ownerId
+                                ? `Chatting with buyer (${chat?.chatUser?.name || 'Loading...'})`
+                                : `Chatting with seller (${chat?.ownerUser?.name || 'Loading...'})`}
+                        </p>
+                    </div>
+
+                    <button onClick={() => dispatch(clearChat())} className='ml-4 p-1 hover:bg-white/20 hover:bg-opacity-20 rounded-lg transition-colors'>
                         <X className='w-5 h-5' />
                     </button>
                 </div>
+
+                {/* Messages Area */}
+                <div className='flex-1 overflow-y-auto p-4 space-y-4 bg-gray-100'>
+                    {isLoading ? (
+                        <div className='flex items-center justify-center h-full'>
+                            <Loader2Icon className='size-6 animate-spin text-indigo-600' />
+                        </div>
+                    ) : messages.length === 0 ? (
+                        <div className='flex items-center justify-center h-full'>
+                            <div className='text-center'>
+                                <p className='text-gray-500 mb-2'>No messages yet</p>
+                                <p className='text-sm text-gray-400'>Start the conversational</p>
+                            </div>
+                        </div>
+                    ) : (
+                        messages.map((message) => (
+                            <div key={message.id} className={`flex ${message.sender_id === user.id ? "justify-end" : "justify-start"}`}>
+                                <div className={`max-w-[70%] rounded-lg p-3 pb-1 ${message.sender_id === user.id ? "bg-indigo-600 text-white" : "bg-white border border-gray-200 text-gray-800"}`}>
+                                    <p className='text-sm break-words whitespace-pre-wrap'>{message.message}</p>
+                                    <p className={`text-[10px] mt-1 ${message.sender_id === user.id ? "text-indigo-200" : "text-gray-400"}`}>{format(new Date(message.createdAt), "MMM dd 'at' h:mm a")}</p>
+                                </div>
+                            </div>
+                        ))
+                    )}
+                    <div ref={messagesEndRef} />
+                </div>
+                {/* Input Area */}
+                {chat?.listing?.status === 'active' ?
+                    (
+                        <form onSubmit={handleSendMessage} className='p-4 bg-white border-t border-gray-200 rounded-b-lg'>
+                            <div className='flex items-end space-x-2'>
+                                <textarea 
+                                value={newMessage}
+                                onChange={(e)=>setNewMessage(e.target.value)}
+                                onKeyDown={(e)=>{
+                                    if(e.key === "Enter" && !e.shiftKey){
+                                        e.preventDefault();
+                                        handleSendMessage(e)
+                                    }
+                                }}
+                                placeholder='Type your message...' className='flex-1 resize-none border border-gray-300 rounded-lg px-4 py-2 focus:outline0indigo-500 max-h-32' rows={1} />
+
+                                <button disabled={!newMessage.trim() || isSending} type='submit' className='bg-indigo-600 hover:bg-indigo-700 text-white p-2.5 rounded-lg disabled:opacity-50 transition-colors'>
+                                    {isLoading ?
+                                    <Loader2Icon className='w-5 h-5 animate-spin'/>
+                                    : <Send className='w-5 h-5'/>}
+                                </button>
+                            </div>
+                        </form>
+                    )
+                    :
+                    (
+                        <div className="p-4 bg-white border-t border-gray-200 rounded-b-lg">
+                            <p className='text-sm text-gray-600 text-center'>{chat ? `Listing is ${chat?.listing?.status}` : "Loading chat..."}</p>
+                        </div>
+                    )
+                }
             </div>
         </div>
     )
--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyChats } from '../assets/assets';
+import { MessageCircle, Search } from 'lucide-react';
 
 const Messages = () => {
+
+  const user = { id: "user_1" };
+
+  const [chats, setChats] = useState([])
+  const [searchQuery, setSearchQuery] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  const fetchUserChats = async () => {
+    setChats(dummyChats)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchUserChats()
+    const interval = setInterval(() => {
+      fetchUserChats();
+    }, 10 * 1000);
+    return () => clearInterval(interval)
+  }, [])
+
   return (
-    <div>Messages</div>
+    <div className='mx-auto min-h-screen px-6 md:px-16 lg:px-24 xl:px-32'>
+      <div className='py-10'>
+        {/* Header */}
+        <div className='mb-8'>
+          <h1 className='text-3xl font-bold text-gray-800 mb-2'>Messages</h1>
+          <p className='text-gray-600'>Chat with buyers and sellers</p>
+        </div>
+
+        {/* search */}
+        <div className='relative max-w-xl mb-8'>
+          <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5' />
+          <input type="text" placeholder='Search conversations...' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className='w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-indigo-500' />
+        </div>
+
+        {/* chat List */}
+        {loading ? (
+          <div className='text-center text-gray-500 py-20'>Loading messages...</div>
+        ) : chats.lenght === 0 ? (
+          <div className='bg-white rounded-lg shadow-xs border border-gray-200 p-16 text-center'>
+            <div className='w-16 h-16 bg-gray-100 rounded-full flex items-center justify-center mx-auto mb-4'>
+              <MessageCircle className='w-8 h-8 text-gray-400' />
+            </div>
+            <h3>{searchQuery ? 'No chats found' : 'No messages yet'}</h3>
+            <p>
+              {searchQuery ? "Try a different search term" : 'Start a conversation by viewing a listing and clicking "Chat with Seller"'}
+            </p>
+          </div>
+        ) : (
+          <div className='bg-white rounded-lg shadow-xs border border-gray-200 divide-y divide-gray-200'>
+            {chats.map((chat)=>{
+              const chatUser = chat.chatUserId === user?.id ? chat.ownerUser : chat.chatUser;
+              return (
+                <button key={chat.Id}>
+
+                </button>
+              )
+            })}
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
